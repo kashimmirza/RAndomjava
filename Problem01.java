4e5ae5b7702b04682e4ad87e11cc88c479f0c186
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-07-01 21:53:43 +0600

    14_01_1811949642.java

---
+++ Problem01.java
@@ -0,0 +1,9 @@
+public class Problem01 {
+          public static void main(String[]arg) {
+        	  
+        	  System.out.println("Welcome to Java");
+        	  System.out.println("Welcome to Computer Science Engineering");
+        	  System.out.println("Programming is fun");
+        	  
+          }
+}
